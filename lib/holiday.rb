d8ce88c455bb902604bc8ac4ab6b08a124985148
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-23 03:31:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
       # else
       #   puts holidaystring
       # end
-      str = holiday
+      str = "#{holiday}"
       if str.include?("_")
         str.gsub!("_"," ")
         str.split.each do |word|
